1494d5d0436e6a9d2ec5166ae571a71165f04f92
Shyam_0373 <shyamtripathi373@gmail.com>
2024-09-29 16:48:21 +0530

    functions1.h and functions1.c file are added

---
+++ functions1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "functions1.h"  // Include the header file for function declarations
+
+// Function implementations
+void function1() {
+    printf("Function 1 executed.\n");
+}
+
+void function2() {
+    printf("Function 2 executed.\n");
+}
+
+void function3() {
+    printf("Function 3 executed.\n");
+}
+
+void function4() {
+    printf("Function 4 executed.\n");
+}
+
+void function5() {
+    printf("Function 5 executed.\n");
+}
+
+// Add more function implementations as needed
+
---
+++ functions1.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS1_H   // Header guard start
+#define FUNCTIONS1_H   // Define the header guard
+
+// Function declarations (prototypes)
+void function1();
+void function2();
+void function3();
+void function4();
+void function5();
+// Add more function declarations as needed
+
+#endif  // Header guard end
+
